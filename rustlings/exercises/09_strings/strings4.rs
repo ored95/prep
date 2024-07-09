651b32c1ea85e7ab4432235c22dca8489b374e03
Alex Nguyen <stepup.ored@gmail.com>
2024-07-09 15:05:58 +0300

    update string: str vs. String

--- rustlings/exercises/09_strings/strings4.rs
+++ rustlings/exercises/09_strings/strings4.rs
@@ -7,8 +7,6 @@
 //
 // No hints this time!
 
-// I AM NOT DONE
-
 fn string_slice(arg: &str) {
     println!("{}", arg);
 }
@@ -17,14 +15,14 @@ fn string(arg: String) {
 }
 
 fn main() {
-    ???("blue");
-    ???("red".to_string());
-    ???(String::from("hi"));
-    ???("rust is fun!".to_owned());
-    ???("nice weather".into());
-    ???(format!("Interpolation {}", "Station"));
-    ???(&String::from("abc")[0..1]);
-    ???("  hello there ".trim());
-    ???("Happy Monday!".to_string().replace("Mon", "Tues"));
-    ???("mY sHiFt KeY iS sTiCkY".to_lowercase());
+    string_slice("blue");                                       // str
+    string("red".to_string());                                  // String (.to_string())
+    string(String::from("hi"));                                 // String
+    string("rust is fun!".to_owned());                          // String (.to_owned())
+    string_slice("nice weather".into());                        // str (.into())
+    string(format!("Interpolation {}", "Station"));             // String (format!)
+    string_slice(&String::from("abc")[0..1]);                   // str (slice array)
+    string_slice("  hello there ".trim());                      // str (.trim())
+    string("Happy Monday!".to_string().replace("Mon", "Tues")); // String (to_string())
+    string("mY sHiFt KeY iS sTiCkY".to_lowercase());            // String (to_lowercase())
 }
