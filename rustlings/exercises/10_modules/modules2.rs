31f932e1351dc238453d869c1251ad987c6511e4
Alex Nguyen <stepup.ored@gmail.com>
2024-07-09 17:37:14 +0300

    update module: keyword `pub`, `use` and `as`

--- rustlings/exercises/10_modules/modules2.rs
+++ rustlings/exercises/10_modules/modules2.rs
@@ -7,13 +7,15 @@
 // Execute `rustlings hint modules2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+// Watch explanation here:
+// https://doc.rust-lang.org/book/ch07-04-bringing-paths-into-scope-with-the-use-keyword.html#re-exporting-names-with-pub-use
+// In this case we need access public to use 
 
 mod delicious_snacks {
     // TODO: Fix these use statements
-    use self::fruits::PEAR as ???
-    use self::veggies::CUCUMBER as ???
-
+    pub use self::fruits::PEAR as fruit;
+    pub use self::veggies::CUCUMBER as veggie;
+    
     mod fruits {
         pub const PEAR: &'static str = "Pear";
         pub const APPLE: &'static str = "Apple";
