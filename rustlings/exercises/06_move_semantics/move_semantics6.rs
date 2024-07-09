04b5e6341664a1eaebea1459b40501d6b46975b9
Alex Nguyen <stepup.ored@gmail.com>
2024-07-09 10:04:07 +0300

    move semantics: operator & and * with &mut keyword to Mutable References

--- rustlings/exercises/06_move_semantics/move_semantics6.rs
+++ rustlings/exercises/06_move_semantics/move_semantics6.rs
@@ -5,22 +5,42 @@
 // Execute `rustlings hint move_semantics6` or use the `hint` watch subcommand
 // for a hint.
 
+// fn main() {
+//     let data = "Rust is great!".to_string();
+
+//     get_char(data.clone());
+
+//     string_uppercase(&data);
+// }
+
+// // Should not take ownership
+// fn get_char(data: String) -> char {
+//     data.chars().last().unwrap()
+// }
+
+// // Should take ownership
+// fn string_uppercase(data: &String) {
+//     let data1 = data.to_uppercase();
+
+//     println!("{}", data1);
+// }
+
 fn main() {
-    let data = "Rust is great!".to_string();
+    let mut data = "Rust is great!".to_string();
 
-    get_char(data.clone());
+    get_char(&data);
 
-    string_uppercase(&data);
+    string_uppercase(&mut data);
 }
 
 // Should not take ownership
-fn get_char(data: String) -> char {
+fn get_char(data: &String) -> char {
     data.chars().last().unwrap()
 }
 
 // Should take ownership
-fn string_uppercase(data: &String) {
-    let data1 = data.to_uppercase();
+fn string_uppercase(data: &mut String) {
+    *data = data.to_uppercase();
 
-    println!("{}", data1);
+    println!("{}", data);
 }
