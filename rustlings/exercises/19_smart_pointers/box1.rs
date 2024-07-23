a0ec196d465e546356944f5f345b51ecbeb145ca
Alex Nguyen <stepup.ored@gmail.com>
2024-07-23 16:09:10 +0300

    update `Box`; smart pointer to store data on the heap

--- rustlings/exercises/19_smart_pointers/box1.rs
+++ rustlings/exercises/19_smart_pointers/box1.rs
@@ -17,12 +17,12 @@
 // Note: the tests should not be changed
 //
 // Execute `rustlings hint box1` or use the `hint` watch subcommand for a hint.
-
-// I AM NOT DONE
+use crate::List::Cons;
+use crate::List::Nil;
 
 #[derive(PartialEq, Debug)]
 pub enum List {
-    Cons(i32, List),
+    Cons(i32, Box<List>),
     Nil,
 }
 
@@ -35,11 +35,11 @@ fn main() {
 }
 
 pub fn create_empty_list() -> List {
-    todo!()
+    Nil
 }
 
 pub fn create_non_empty_list() -> List {
-    todo!()
+    Cons(0, Box::new(Cons(1, Box::new(Cons(2, Box::new(Nil))))))
 }
 
 #[cfg(test)]
